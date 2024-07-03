c1f7a36e6fa3a1855c8fb80ed3bcc56b23c3a976
Surya-Prasad06 <suryaprasadsadige007@gmail.com>
2024-07-03 08:56:13 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,120 @@
+const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
+const questionContainerElement = document.getElementById('question-container');
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answer-buttons');
+const scoreContainer = document.getElementById('score-container');
+const scoreElement = document.getElementById('score');
+
+let shuffledQuestions, currentQuestionIndex;
+let score = 0;
+
+startButton.addEventListener('click', startGame);
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++;
+    setNextQuestion();
+});
+
+function startGame() {
+    startButton.classList.add('hide');
+    score = 0;
+    scoreElement.innerText = score;
+    scoreContainer.classList.add('hide');
+    shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+    currentQuestionIndex = 0;
+    questionContainerElement.classList.remove('hide');
+    setNextQuestion();
+}
+
+function setNextQuestion() {
+    resetState();
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question;
+    question.answers.forEach(answer => {
+        const button = document.createElement('button');
+        button.innerText = answer.text;
+        button.classList.add('btn');
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener('click', selectAnswer);
+        answerButtonsElement.appendChild(button);
+    });
+}
+
+function resetState() {
+    clearStatusClass(document.body);
+    nextButton.classList.add('hide');
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+    }
+}
+
+function selectAnswer(e) {
+    const selectedButton = e.target;
+    const correct = selectedButton.dataset.correct === 'true';
+    if (correct) {
+        score++;
+        scoreElement.innerText = score;
+    }
+    setStatusClass(document.body, correct);
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct === 'true');
+    });
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide');
+    } else {
+        startButton.innerText = 'Restart';
+        startButton.classList.remove('hide');
+        scoreContainer.classList.remove('hide');
+    }
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element);
+    if (correct) {
+        element.classList.add('correct');
+    } else {
+        element.classList.add('wrong');
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct');
+    element.classList.remove('wrong');
+}
+
+const questions = [
+    {
+        question: 'What is 2 + 2?',
+        answers: [
+            { text: '4', correct: true },
+            { text: '22', correct: false }
+        ]
+    },
+    {
+        question: 'Who is the president of the USA?',
+        answers: [
+            { text: 'Donald Trump', correct: false },
+            { text: 'Joe Biden', correct: true }
+        ]
+    },
+    {
+        question: 'What is the capital of France?',
+        answers: [
+            { text: 'Berlin', correct: false },
+            { text: 'Paris', correct: true }
+        ]
+    },
+    {
+        question: 'What does HTML stand for?',
+        answers: [
+            { text: 'HyperText Markup Language', correct: true },
+            { text: 'Hot Mail', correct: false },
+            { text: 'How to Make Lasagna', correct: false }
+        ]
+    }
+];
